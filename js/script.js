bb657a1aebdb1bf562d0b118fc2fda343fe6e7c6
Fahrur Razi <fahrurben@gmail.com>
2020-12-12 17:27:13 +0700

    Add Invoice me project

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $ = jQuery;
 $( document ).ready(function() {
 	initPhotoSwipeFromDOM('#my-gallery');
 	initPhotoSwipeFromDOM('#udigital-gallery');
+	initPhotoSwipeFromDOM('#invoice-me-gallery');
 
 	$( "#show12u12" ).click(function() {
 		$( "#my-gallery" ).toggle( "slow", function() {
@@ -12,6 +13,12 @@ $( document ).ready(function() {
 
 	$( "#showudigital" ).click(function() {
 		$( "#udigital-gallery" ).toggle( "slow", function() {
+			// Animation complete.
+		});
+	});
+
+	$( "#showInvoice" ).click(function() {
+		$( "#invoice-me-gallery" ).toggle( "slow", function() {
 			// Animation complete.
 		});
 	});
